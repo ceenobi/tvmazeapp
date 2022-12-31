1ba7220f66616fbe7a4b49d3450a39340899d30c
ceenobi <cobimbachu@gmail.com>
2022-12-31 14:00:29 +0100

    fix component

--- src/components/ActionTV.jsx
+++ src/components/ActionTV.jsx
@@ -14,36 +14,38 @@ export default function ActionTV({ error, loading, shows }) {
   if (loading) {
     return <Spinner />
   } else if (error || shows) {
-    ;<div className='mb-12'>
-      <p className='font-graphik uppercase mb-4 font-bold tracking-wide font-lighter'>
-        action
-      </p>
-      {error && <p>{error.message}</p>}
-      <div className='relative'>
-        <div
-          className='flex overflow-x-scroll overflow-y-hidden scroll-smooth'
-          ref={scrollRef}
-        >
-          {actionTv.map((action) => (
-            <MiniMediaCard key={action.id} {...action} />
-          ))}
-        </div>
-        <div className='hidden md:block absolute top-24 w-full'>
-          <div className='flex justify-between items-center'>
-            <IoMdArrowDropleftCircle
-              className='text-white cursor-pointer'
-              size='3.2rem'
-              onClick={() => scroll('left')}
-            />
-            <IoMdArrowDroprightCircle
-              className='text-white cursor-pointer'
-              size='3.2rem'
-              onClick={() => scroll('right')}
-            />
+    return (
+      <div className='mb-12'>
+        <p className='font-graphik uppercase mb-4 font-bold tracking-wide font-lighter'>
+          action
+        </p>
+        {error && <p>{error.message}</p>}
+        <div className='relative'>
+          <div
+            className='flex overflow-x-scroll overflow-y-hidden scroll-smooth'
+            ref={scrollRef}
+          >
+            {actionTv.map((action) => (
+              <MiniMediaCard key={action.id} {...action} />
+            ))}
+          </div>
+          <div className='hidden md:block absolute top-24 w-full'>
+            <div className='flex justify-between items-center'>
+              <IoMdArrowDropleftCircle
+                className='text-white cursor-pointer'
+                size='3.2rem'
+                onClick={() => scroll('left')}
+              />
+              <IoMdArrowDroprightCircle
+                className='text-white cursor-pointer'
+                size='3.2rem'
+                onClick={() => scroll('right')}
+              />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    )
   }
 
   return <p className='mt-20 p-10'>Something went wrong</p>
