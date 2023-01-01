64897979c6b59b8f9d60dc798e031ce0944b6aa8
ceenobi <cobimbachu@gmail.com>
2023-01-01 09:54:26 +0100

    handle err

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 export default function Error() {
   return (
@@ -8,11 +7,13 @@ export default function Error() {
         <h1 className='font-graphik text-2xl mb-6'>
           Ooops! an error has occurred
         </h1>
-        <Link to='/'>
-          <button className='border-none rounded-md bg-slate-900 text-zinc-300 w-[150px] p-2'>
-            Go Home
-          </button>
-        </Link>
+
+        <button
+          className='border-none rounded-md bg-slate-900 text-zinc-300 w-[150px] p-2'
+          onClick={() => location.reload()}
+        >
+          Retry
+        </button>
       </div>
     </div>
   )
